50a473bff71848f4b2d1171949a874cd10c0c07a
黄好杰 <13612989029@163.com>
2020-11-02 17:24:41 +0800

    班列要闻

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,18 +1,18 @@
 module.exports = {
     "zh-cn": {
-        "new": '品高快报',
+        "new": '班列要闻',
         "All": '更多',
         "NoneData": "暂无新闻",
         "ErrorLoadData": "加载失败"
     },
     "zh-hk": {
-        "new": '品高快报',
+        "new": '班列要闻',
         "All": '更多',
         "NoneData": "暫無新聞",
         "ErrorLoadData": "加載失敗"
     },
     "en": {
-        "new": 'Bingo News',
+        "new": '班列要闻',
         "All": 'More',
         "NoneData": "No data",
         "ErrorLoadData": "loading failed"
